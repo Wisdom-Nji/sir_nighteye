85dae715c374e2169459e3e89d3c79460a5f9129
Sherlock Wisdom <wisdomnji@gmail.com>
2020-08-26 23:13:06 +0100

    created new links for parser

--- parser.py
+++ parser.py
@@ -8,6 +8,8 @@ class customHTMLParser( HTMLParser ):
 	def handle_data(self, data):
 		print( "Encountered some data: ", data)
 
+# Study this link, will tell you how to retrieve the information
+# https://search.grants.nih.gov/guide/api/data?perpage=25&sort=reldate:desc&from=0&type=active,nosis&parentic=all&primaryic=all&activitycodes=all&doctype=all&parentfoa=all&daterange=08262020-08262020&clinicaltrials=all&fields=all&spons=true&query=
 htmlFile = open( "sample_html.html", "r")
 parser = customHTMLParser()
 parser.feed( htmlFile.read() )
