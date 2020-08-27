1023511be0dbce01b00c0a2bf40e92ecd5d814b9
Sherlock Wisdom <wisdomnji@gmail.com>
2020-08-27 13:20:48 +0100

    customized script for NIH

--- nih_parser.py
+++ nih_parser.py
@@ -10,7 +10,8 @@ class customHTMLParser( HTMLParser ):
 		print( "Encountered some data: ", data)
 
 # Study this link, will tell you how to retrieve the information
-nih_search_string = "https://search.grants.nih.gov/guide/api/data?perpage=25&sort=reldate:desc&from=0&type=active,nosis&parentic=all&primaryic=all&activitycodes=all&doctype=all&parentfoa=all&daterange=08262020-08262020&clinicaltrials=all&fields=all&spons=true&query="
+# nih_search_string = "https://search.grants.nih.gov/guide/api/data?perpage=25&sort=reldate:desc&from=0&type=active,nosis&parentic=all&primaryic=all&activitycodes=all&doctype=all&parentfoa=all&daterange=08262020-08262020&clinicaltrials=all&fields=all&spons=true&query="
+nih_search_string = "https://search.grants.nih.gov/guide/api/data?perpage=1000&sort=expdate:asc&from=0&type=active,notices,nosis&parentic=all&primaryic=all&activitycodes=all&doctype=all&parentfoa=all&daterange=01021991-08272020&clinicaltrials=all&fields=all&spons=true&query="
 
 # htmlFile = open( "sample_html.html", "r")
 htmlRequest = htmlRequest.get( nih_search_string )
@@ -19,7 +20,7 @@ htmlRequest_json = htmlRequest.json()
 
 hits = htmlRequest_json["data"]["hits"]["hits"]
 for i in range(len(hits)):
-	print( "Title ", i, ": ", hits[i]["_source"]["title"])
+	print( "Title", i, ":", hits[i]["_source"]["title"])
 	
 # parser = customHTMLParser()
 # parser.feed( htmlFile.read() )
